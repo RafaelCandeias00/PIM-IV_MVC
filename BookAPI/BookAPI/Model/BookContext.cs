47918dd398f45c2a5db642ce1bc6f3299ed3aa4d
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-03-30 22:17:11 -0300

    BookAPI

---
+++ BookAPI/BookAPI/Model/BookContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Model
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions options) : base(options) {
+            Database.EnsureCreated(); 
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
